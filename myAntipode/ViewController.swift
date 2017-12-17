5a203b9a1bb94ba68f20c530eed1a97d0b5b703f
Ryan Wright <ryanw@care2team.com>
2017-12-17 14:52:07 -0800

    Let’s get it started.

--- myAntipode/ViewController.swift
+++ myAntipode/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let regionRadius: CLLocationDistance = 1000
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //TODO:Set up the location manager here.
+        
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        centerMapOnLocation(location: initialLocation)
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            print("before lat: \(location.coordinate.latitude), long: \(location.coordinate.longitude)")
+            
+            var newLat : Double = 0.0
+            var newLong : Double = 0.0
+            
+            newLat = location.coordinate.latitude * -1.0
+            
+            newLong = location.coordinate.longitude
+            
+            newLong = 180.0 - abs(location.coordinate.longitude)
+            
+            if location.coordinate.longitude > 0 {
+                newLong = newLong * -1.0
+            }
+            
+            print("after lat: \(newLat), long: \(newLong)")
+            centerMapOnLocation(location: CLLocation(latitude: newLat, longitude: newLong))
+        }
+    }
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 
 }
 
