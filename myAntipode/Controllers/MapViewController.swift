67162a4d3d5829eb9f5d115a0731c20dd16f0ed0
Ryan Wright <rywright@gmail.com>
2017-12-31 11:52:34 -0800

    Fixed lots of stuff in MapViewController. Increased map region radius

--- myAntipode/Controllers/MapViewController.swift
+++ myAntipode/Controllers/MapViewController.swift
@@ -5,91 +5,60 @@ import MapKit
 class MapViewController: UIViewController, CLLocationManagerDelegate {
     
     var selectedSavedLocation : SavedLocation?
+
+    let regionRadius: CLLocationDistance = 500000
     
-    let regionRadius: CLLocationDistance = 1000
     let locationManager = CLLocationManager()
-    var centerOnMyLocation : Bool = false
-    
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("LocationHistoryItems.plist")
+    var centerOnAntipode : Bool = true
     
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.mapType = MKMapType.hybrid
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
+        let scale = MKScaleView(mapView: mapView)
+        scale.scaleVisibility = .visible
+        view.addSubview(scale)
+        
+        // Run this if the selectedSavedLocation set before the previous segue
+        // is actually just the current location.
+        if 1 == 2 {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        } else {
+            centerMapOnLocation(location: getLocationForMap(latitude: selectedSavedLocation!.latitude, longitude: selectedSavedLocation!.longitude))
+        }
     }
 
+    func getLocationForMap(latitude: Double, longitude: Double) -> CLLocation {
+        var newLat : Double = latitude
+        var newLong : Double = longitude
+
+        if centerOnAntipode {
+            newLat = latitude * -1.0
+            newLong = longitude
+            newLong = 180.0 - abs(longitude)
+            if longitude > 0 {
+                newLong = newLong * -1.0
+            }
+        }
+        
+        return  CLLocation(latitude: newLat, longitude: newLong)
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[locations.count - 1]
         if location.horizontalAccuracy > 0 {
-            
             locationManager.stopUpdatingLocation()
-            var newLat : Double = 0.0
-            var newLong : Double = 0.0
-            
-            if centerOnMyLocation {
-                newLat = location.coordinate.latitude
-                newLong = location.coordinate.longitude
-            } else {
-                newLat = location.coordinate.latitude * -1.0
-                
-                newLong = location.coordinate.longitude
-                
-                newLong = 180.0 - abs(location.coordinate.longitude)
-                
-                if location.coordinate.longitude > 0 {
-                    newLong = newLong * -1.0
-                }
-            }
-            centerMapOnLocation(location: CLLocation(latitude: newLat, longitude: newLong))
+            centerMapOnLocation(location: getLocationForMap(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude))
         }
     }
+
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
         mapView.setRegion(coordinateRegion, animated: true)
     }
-
-    @IBAction func myLocationButtonPressed(_ sender: Any) {
-        centerOnMyLocation = true
-        locationManager.startUpdatingLocation()
-    }
-    
-    @IBAction func myAntipodeButtonPressed(_ sender: Any) {
-        centerOnMyLocation = false
-        locationManager.startUpdatingLocation()
-    }
-    
-    func loadItems() {
-        /*
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                locationHistoryItemArray = try decoder.decode([LocationHistoryItem].self, from: data)
-            } catch {
-                print("Error decoding item array, \(error)")
-            }
-        }
-        print(locationHistoryItemArray)
- */
-    }
-    
-    func saveItems() {
-        /*
-        let encoder = PropertyListEncoder()
-        do {
-            let data = try encoder.encode(locationHistoryItemArray)
-            try data.write(to: dataFilePath!)
-        } catch {
-            print("Error encoding item array, \(error)")
-        }
-        // self.tableView.reloadData()
- */
-    }
-
     
 }
 
